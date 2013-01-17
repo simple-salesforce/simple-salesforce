3cd54e356ca5c2240e47eb3902b7312a30270b66
Nick Catalano <nickcatal@gmail.com>
2013-01-17 18:17:19 -0500

    Using .format and '+' instead of %s notation

--- simple_salesforce/api.py
+++ simple_salesforce/api.py
@@ -25,11 +25,11 @@ class SalesforceAPI(object):
         return self._raw_search(search_string)
 
     def quick_search(self, search):
-        search_string = urllib.quote_plus('FIND {%s}' % search)
+        search_string = urllib.quote_plus('FIND {{search_string}}'.format(search_string=search))
         return self._raw_search(search_string)
 
     def _raw_search(self, search_string):
-        result = requests.get('https://%s/services/data/v26.0/search/?q=%s' % (self.sfInstance, search_string), headers=self.headers)
+        result = requests.get('https://{instance}/services/data/v26.0/search/?q={query}'.format(instance=self.sfInstance,query=search_string), headers=self.headers)
         if result.status_code != 200:
             raise SalesforceGeneralError(result.content)
         json_result = result.json()
@@ -41,13 +41,13 @@ class SalesforceAPI(object):
     # Query Handler
     def query(self, query):
         query_string = urllib.quote_plus(query)
-        result = requests.get('https://%s/services/data/v20.0/query/?q=%s' % (self.sfInstance, query_string), headers=self.headers)
+        result = requests.get('https://{instance}/services/data/v20.0/query/?q={query}'.format(instance=self.sfInstance,query=query_string), headers=self.headers)
         if result.status_code != 200:
             raise SalesforceGeneralError(result.content)
         return result.json()
 
     def query_more(self, nextRecordsId):
-        result = requests.get('https://%s/services/data/v20.0/query/%s' % nextRecordsId, headers=self.headers)
+        result = requests.get('https://{instance}/services/data/v20.0/query/{next_record_id}'.format(instance=self.sfInstance,next_record_id=nextRecordsId), headers=self.headers)
         if result.status_code != 200:
             raise SalesforceGeneralError(result.content)
         return result.json()
@@ -59,18 +59,18 @@ class SObject(object):
     def __init__(self, objectName, sessionId, sfInstance):
         self.sessionid = sessionId
         self.name = objectName
-        self.baseurl = 'https://%s/services/data/v20.0/sobjects/%s/' % (sfInstance, objectName)
+        self.baseurl = 'https://{instance}/services/data/v20.0/sobjects/{object_name}/'.format(instance=sfInstance, object_name=objectName)
 
     def metadata(self):
         result = self._call_salesforce('GET',self.baseurl)
         return result.json()
 
     def describe(self):
-        result = self._call_salesforce('GET','%sdescribe' % self.baseurl)
+        result = self._call_salesforce('GET',self.baseurl + 'describe')
         return result.json()
 
     def get(self,recordId):
-        result = self._call_salesforce('GET','%s%s' % (self.baseurl, recordId))
+        result = self._call_salesforce('GET',self.baseurl + recordId)
         return result.json()
 
     def create(self,data):
@@ -78,15 +78,15 @@ class SObject(object):
         return result.json()
 
     def upsert(self,recordId,data):
-        result = self._call_salesforce('PATCH','%s%s' % (self.baseurl, recordId), data=json.dumps(data))
+        result = self._call_salesforce('PATCH',self.baseurl + recordId, data=json.dumps(data))
         return result.status_code
 
     def update(self,recordId,data):
-        result = self._call_salesforce('PATCH','%s%s' % (self.baseurl, recordId), data=json.dumps(data))
+        result = self._call_salesforce('PATCH',self.baseurl + recordId, data=json.dumps(data))
         return result.status_code
 
     def delete(self, recordId):
-        result = self._call_salesforce('DELETE','%s%s' % (self.baseurl, recordId))
+        result = self._call_salesforce('DELETE',self.baseurl + recordId)
         return result.status_code
 
     def _call_salesforce(self, method, url, **kwargs):
