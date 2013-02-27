aa7667a232e846399349209303a43567edcce774
Greg Unrein <greg@cedexis.com>
2013-02-27 14:13:59 -0800

    PEP8 code changes and a little de-duping around the soapUrl

--- simple_salesforce/login.py
+++ simple_salesforce/login.py
@@ -5,17 +5,19 @@ Heavily Modified from RestForce 1.0.0
 from util import getUniqueElementValueFromXmlString
 import requests
 
-def login(username, password, securityToken, sandbox=False,
-          sf_version="23.0"):
-    if sandbox:
-        soapUrl = "https://test.salesforce.com/services/Soap/u/" + sf_version
-    else:
-        soapUrl = "https://login.salesforce.com/services/Soap/u/" + sf_version
 
-    loginSoapRequestBody = """<?xml version="1.0" encoding="utf-8" ?> 
-        <env:Envelope 
-            xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
-            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+def login(username, password, securityToken, sandbox=False,
+          sf_version='23.0'):
+    soapUrl = 'https://{domain}.salesforce.com/services/Soap/u/{sf_version}'
+    domain = 'test'
+    if not sandbox:
+        domain = 'login'
+    soapUrl = soapUrl.format(domain=domain, sf_version=sf_version)
+
+    loginSoapRequestBody = """<?xml version="1.0" encoding="utf-8" ?>
+        <env:Envelope
+            xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
             xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">
             <env:Body>
                 <n1:login xmlns:n1="urn:partner.soap.sforce.com">
@@ -26,24 +28,34 @@ def login(username, password, securityToken, sandbox=False,
         </env:Envelope>""" % (username, password, securityToken)
 
     loginSoapRequestHeaders = {
-        "content-type":"text/xml",
-        "charset":"UTF-8",
-        "SOAPAction":"login"
+        'content-type': 'text/xml',
+        'charset': 'UTF-8',
+        'SOAPAction': 'login'
     }
-    response = requests.post(soapUrl, loginSoapRequestBody, headers=loginSoapRequestHeaders)
+    response = requests.post(soapUrl, loginSoapRequestBody,
+                             headers=loginSoapRequestHeaders)
 
     if response.status_code != 200:
-        exception_code = getUniqueElementValueFromXmlString(response.content, 'sf:exceptionCode')
-        exception_message = getUniqueElementValueFromXmlString(response.content, 'sf:exceptionMessage')
-        raise SalesforceAuthenticationFailed('%s: %s' % (exception_code, exception_message)) 
+        except_code = getUniqueElementValueFromXmlString(response.content,
+                                                         'sf:exceptionCode')
+        except_msg = getUniqueElementValueFromXmlString(response.content,
+                                                        'sf:exceptionMessage')
+        raise SalesforceAuthenticationFailed('%s: %s' % (except_code,
+                                                         except_msg))
 
-    sessionId = getUniqueElementValueFromXmlString(response.content, 'sessionId')
-    serverUrl = getUniqueElementValueFromXmlString(response.content, 'serverUrl')
-    sfInstance = serverUrl.replace('http://', '').replace('https://', '').split('/')[0].replace('-api', '')
+    sessionId = getUniqueElementValueFromXmlString(response.content,
+                                                   'sessionId')
+    serverUrl = getUniqueElementValueFromXmlString(response.content,
+                                                   'serverUrl')
+    sfInstance = (serverUrl
+                  .replace('http://', '')
+                  .replace('https://', '')
+                  .split('/')[0]
+                  .replace('-api', ''))
 
     return (sessionId, sfInstance)
 
-    
+
 class SalesforceAuthenticationFailed(Exception):
     '''
     Thrown to indicate that authentication with Salesforce failed.
