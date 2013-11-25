ef031b7f4df869776ac78c3899672d85411dd7fb
Nick Catalano <nickcatal@gmail.com>
2013-11-24 22:54:19 -0600

    setup.py pep8 cleanup & pylint rcfile

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+"""Simple-Salesforce Package Setup"""
+
 from setuptools import setup
 import textwrap
 import sys
 
 extra_install_requires = []
-extra_tests_require=[]
+extra_tests_require = []
 if sys.version_info < (2, 7):
     extra_install_requires.append('ordereddict>=1.1')
     extra_tests_require.append('unittest2>=0.5.1')
@@ -16,7 +18,9 @@ setup(
     packages=['simple_salesforce',],
     url='https://github.com/neworganizing/simple-salesforce',
     license='APACHE',
-    description="Simple Salesforce is a basic Salesforce.com REST API client. The goal is to provide a very low-level interface to the API, returning a dictionary of the API JSON response.",
+    description=("Simple Salesforce is a basic Salesforce.com REST API client. "
+        "The goal is to provide a very low-level interface to the API, "
+        "returning a dictionary of the API JSON response."),
     long_description=textwrap.dedent(open('README.rst', 'r').read()),
     install_requires=[
         'requests',
@@ -27,5 +31,12 @@ setup(
     ] + extra_tests_require,
     test_suite = 'nose.collector',
     keywords = "python salesforce salesforce.com",
-    classifiers=['Development Status :: 4 - Beta', 'Environment :: Console', 'Intended Audience :: Developers', 'Natural Language :: English', 'Operating System :: OS Independent', 'Topic :: Internet :: WWW/HTTP']
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
 )
