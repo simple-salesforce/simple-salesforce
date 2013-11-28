f536f64197e249917beb9b19bcf8a0b08644d9e3
Nick Catalano <nickcatal@gmail.com>
2013-11-28 02:47:09 -0600

    Additional test pep8/pylint cleanup
    Cleaning up the module itself could be a pain, but no reason not to keep the tests reasonably compliant

--- simple_salesforce/tests/test_login.py
+++ simple_salesforce/tests/test_login.py
@@ -12,10 +12,15 @@ except ImportError:
     # Python 3
     from unittest.mock import Mock, patch
 
-from simple_salesforce.login import SalesforceLogin, SalesforceAuthenticationFailed
+from simple_salesforce.login import (
+    SalesforceLogin, SalesforceAuthenticationFailed
+)
 
-class TestExceptionHandler(unittest.TestCase):
+
+class TestSalesforceLogin(unittest.TestCase):
+    """Tests for the SalesforceLogin function"""
     def setUp(self):
+        """Setup the SalesforceLogin tests"""
         request_patcher = patch('simple_salesforce.login.requests')
         self.mockrequest = request_patcher.start()
         self.addCleanup(request_patcher.stop)
@@ -24,6 +29,7 @@ class TestExceptionHandler(unittest.TestCase):
         """Test A Failed Login Response"""
         return_mock = Mock()
         return_mock.status_code = 500
+        # pylint: disable=line-too-long
         return_mock.content = '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sf="urn:fault.partner.soap.sforce.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Body><soapenv:Fault><faultcode>INVALID_LOGIN</faultcode><faultstring>INVALID_LOGIN: Invalid username, password, security token; or user locked out.</faultstring><detail><sf:LoginFault xsi:type="sf:LoginFault"><sf:exceptionCode>INVALID_LOGIN</sf:exceptionCode><sf:exceptionMessage>Invalid username, password, security token; or user locked out.</sf:exceptionMessage></sf:LoginFault></detail></soapenv:Fault></soapenv:Body></soapenv:Envelope>'
         self.mockrequest.post.return_value = return_mock
 
