117626c70e4d7d00484e719ec98e149fb8e572c8
Nick Catalano <nickcatal@gmail.com>
2013-12-04 10:40:40 -0600

    Updated setup.py
    Due to the way pypi prioritizes packages until version 1.0 we'll need to use 0.xx.x

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ if sys.version_info < (3, 0):
 
 setup(
     name='simple-salesforce',
-    version='0.61',
+    version='0.62.0',
     author='Nick Catalano',
     packages=['simple_salesforce',],
     url='https://github.com/neworganizing/simple-salesforce',
-    license='APACHE',
+    license='Apache 2.0',
     description=("Simple Salesforce is a basic Salesforce.com REST API client. "
         "The goal is to provide a very low-level interface to the API, "
-        "returning a dictionary of the API JSON response."),
+        "returning an ordered dictionary of the API JSON response."),
     long_description=textwrap.dedent(open('README.rst', 'r').read()),
     install_requires=[
         'requests'
@@ -34,10 +34,15 @@ setup(
     keywords = "python salesforce salesforce.com",
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
-        'Natural Language :: English',
+        'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP'
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ]
 )
