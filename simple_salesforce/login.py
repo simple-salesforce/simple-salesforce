2606f870f154c1942a45d092c650c245daf3e2e4
Nicolas Bessi (nbessi) <nbessi@users.noreply.github.com>
2014-12-09 16:37:09 +0100

    Authenitcation error derive form `SalesforceError`

--- simple_salesforce/login.py
+++ simple_salesforce/login.py
@@ -4,6 +4,7 @@ Heavily Modified from RestForce 1.0.0
 """
 
 from simple_salesforce.util import getUniqueElementValueFromXmlString
+from simple_salesforce.api import SalesforceError
 try:
     # Python 3+
     from html import escape
@@ -126,7 +127,7 @@ def SalesforceLogin(**kwargs):
     return session_id, sf_instance
 
 
-class SalesforceAuthenticationFailed(Exception):
+class SalesforceAuthenticationFailed(SalesforceError):
     """
     Thrown to indicate that authentication with Salesforce failed.
     """
