cd55fa1538f2b5fba8f20fef5f7390d4a05b5e5d
Nick Catalano <nickcatal@gmail.com>
2015-01-22 21:44:22 -0600

    Updating setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name='simple-salesforce',
-    version='0.66.1',
+    version='0.66.2',
     author='Nick Catalano',
     packages=['simple_salesforce',],
     url='https://github.com/neworganizing/simple-salesforce',
