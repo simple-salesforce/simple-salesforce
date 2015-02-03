db6ae3c6c2f547a53118c05bad17cb98131df8ac
Nick Catalano <nickcatal@gmail.com>
2015-02-02 20:52:08 -0600

    Updating setup.py for PyPI upload

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name='simple-salesforce',
-    version='0.66.2',
+    version='0.67.0',
     author='Nick Catalano',
     packages=['simple_salesforce',],
     url='https://github.com/neworganizing/simple-salesforce',
