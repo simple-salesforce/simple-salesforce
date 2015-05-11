1bd574ea10cba8ed54f17edaa3adbf7bab08de8f
olga <olgako1@gmail.com>
2015-05-11 09:01:07 +0300

    incremented version after adding get_by_custom_id

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name='simple-salesforce',
-    version='0.67.1',
+    version='0.67.2',
     author='Nick Catalano',
     packages=['simple_salesforce',],
     url='https://github.com/neworganizing/simple-salesforce',
