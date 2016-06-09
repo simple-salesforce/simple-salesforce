5470b0751b03c554a75e9feb4d3767a3c6a1887e
Demian Brecht <dbrecht@salesforce.com>
2016-06-09 12:20:03 -0700

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name='simple-salesforce',
-    version='0.68.2',
+    version='0.69',
     author='Nick Catalano',
     packages=['simple_salesforce',],
     url='https://github.com/simple-salesforce/simple-salesforce',
