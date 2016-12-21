a1f6fd691d7b0c2b58c231aa8f4da2111b6029dd
colemanja91 <colemanja91@gmail.com>
2016-12-21 11:50:57 -0500

    line-length edits to appease the pylint gods

--- simple_salesforce/bulk.py
+++ simple_salesforce/bulk.py
@@ -13,8 +13,10 @@ except ImportError:
 
 class SFBulkHandler(object):
     """ Bulk API request handler
-    Intermediate class which allows us to use commands such as 'sf.bulk.Contacts.create(...)'
-    This is really just a middle layer, whose sole purpose is to allow the above syntax
+    Intermediate class which allows us to use commands,
+     such as 'sf.bulk.Contacts.create(...)'
+    This is really just a middle layer, whose sole purpose is
+    to allow the above syntax
     """
 
     def __init__(self, session_id, bulk_url, proxies=None, session=None):
@@ -36,7 +38,8 @@ class SFBulkHandler(object):
         if not session and proxies is not None:
             self.session.proxies = proxies
 
-        # Define these headers separate from Salesforce class, as bulk uses a slightly different format
+        # Define these headers separate from Salesforce class,
+        # as bulk uses a slightly different format
         self.headers = {
             'Content-Type': 'application/json',
             'X-SFDC-Session': self.session_id,
@@ -44,7 +47,8 @@ class SFBulkHandler(object):
         }
 
     def __getattr__(self, name):
-        return SFBulkType(object_name=name, bulk_url=self.bulk_url, headers=self.headers, session=self.session)
+        return SFBulkType(object_name=name, bulk_url=self.bulk_url,
+                          headers=self.headers, session=self.session)
 
 class SFBulkType(object):
     """ Interface to Bulk/Async API functions"""
@@ -88,7 +92,8 @@ class SFBulkType(object):
 
         url = "{}{}".format(self.bulk_url, 'job')
 
-        result = _call_salesforce(url=url, method='POST', session=self.session, headers=self.headers, data=json.dumps(payload))
+        result = _call_salesforce(url=url, method='POST', session=self.session,
+                                headers=self.headers, data=json.dumps(payload))
         return result.json(object_pairs_hook=OrderedDict)
 
     def _close_job(self, job_id):
@@ -100,7 +105,8 @@ class SFBulkType(object):
 
         url = "{}{}{}".format(self.bulk_url, 'job/', job_id)
 
-        result = _call_salesforce(url=url, method='POST', session=self.session, headers=self.headers, data=json.dumps(payload))
+        result = _call_salesforce(url=url, method='POST', session=self.session,
+                                headers=self.headers, data=json.dumps(payload))
         return result.json(object_pairs_hook=OrderedDict)
 
     def _get_job(self, job_id):
@@ -108,12 +114,14 @@ class SFBulkType(object):
 
         url = "{}{}{}".format(self.bulk_url, 'job/', job_id)
 
-        result = _call_salesforce(url=url, method='GET', session=self.session, headers=self.headers)
+        result = _call_salesforce(url=url, method='GET', session=self.session,
+                                  headers=self.headers)
         return result.json(object_pairs_hook=OrderedDict)
 
     def _add_batch(self, job_id, data, operation):
         """ Add a set of data as a batch to an existing job
-        Separating this out in case of later implementations involving multiple batches
+        Separating this out in case of later
+        implementations involving multiple batches
         """
 
         url = "{}{}{}{}".format(self.bulk_url, 'job/', job_id, '/batch')
@@ -121,33 +129,41 @@ class SFBulkType(object):
         if operation != 'query':
             data = json.dumps(data)
 
-        result = _call_salesforce(url=url, method='POST', session=self.session, headers=self.headers, data=data)
+        result = _call_salesforce(url=url, method='POST', session=self.session,
+                                  headers=self.headers, data=data)
         return result.json(object_pairs_hook=OrderedDict)
 
     def _get_batch(self, job_id, batch_id):
         """ Get an existing batch to check the status """
 
-        url = "{}{}{}{}{}".format(self.bulk_url, 'job/', job_id, '/batch/', batch_id)
+        url = "{}{}{}{}{}".format(self.bulk_url, 'job/',
+                                  job_id, '/batch/', batch_id)
 
-        result = _call_salesforce(url=url, method='GET', session=self.session, headers=self.headers)
+        result = _call_salesforce(url=url, method='GET', session=self.session,
+                                  headers=self.headers)
         return result.json(object_pairs_hook=OrderedDict)
 
     def _get_batch_results(self, job_id, batch_id, operation):
         """ retrieve a set of results from a completed job """
 
-        url = "{}{}{}{}{}{}".format(self.bulk_url, 'job/', job_id, '/batch/', batch_id, '/result')
+        url = "{}{}{}{}{}{}".format(self.bulk_url, 'job/', job_id, '/batch/',
+                                    batch_id, '/result')
 
-        result = _call_salesforce(url=url, method='GET', session=self.session, headers=self.headers)
+        result = _call_salesforce(url=url, method='GET', session=self.session,
+                                  headers=self.headers)
 
         if operation == 'query':
             url_query_results = "{}{}{}".format(url, '/', result.json()[0])
-            query_result = _call_salesforce(url=url_query_results, method='GET', session=self.session, headers=self.headers)
+            query_result = _call_salesforce(url=url_query_results, method='GET',
+                                    session=self.session, headers=self.headers)
             return query_result.json()
 
         return result.json()
 
-    def _bulk_operation(self, object_name, operation, data, external_id_field=None, wait=5):
-        """ String together helper functions to create a complete end-to-end bulk API request
+    def _bulk_operation(self, object_name, operation, data,
+                        external_id_field=None, wait=5):
+        """ String together helper functions to create a complete
+        end-to-end bulk API request
 
         Arguments:
 
@@ -158,53 +174,68 @@ class SFBulkType(object):
         * wait -- seconds to sleep between checking batch status
         """
 
-        job = self._create_job(object_name=object_name, operation=operation, external_id_field=external_id_field)
+        job = self._create_job(object_name=object_name, operation=operation,
+                               external_id_field=external_id_field)
 
-        batch = self._add_batch(job_id=job['id'], data=data, operation=operation)
+        batch = self._add_batch(job_id=job['id'], data=data,
+                                operation=operation)
 
         self._close_job(job_id=job['id'])
 
-        batch_status = self._get_batch(job_id=batch['jobId'], batch_id=batch['id'])['state']
+        batch_status = self._get_batch(job_id=batch['jobId'],
+                                       batch_id=batch['id'])['state']
 
         while batch_status not in ['Completed', 'Failed', 'Not Processed']:
             sleep(wait)
-            batch_status = self._get_batch(job_id=batch['jobId'], batch_id=batch['id'])['state']
+            batch_status = self._get_batch(job_id=batch['jobId'],
+                                           batch_id=batch['id'])['state']
 
-        results = self._get_batch_results(job_id=batch['jobId'], batch_id=batch['id'], operation=operation)
+        results = self._get_batch_results(job_id=batch['jobId'],
+                                    batch_id=batch['id'], operation=operation)
         return results
 
-    # Wrappers for _bulk_operation which expose the supported Salesforce bulk operations
+    # _bulk_operation wrappers to expose supported Salesforce bulk operations
     def delete(self, data):
         """ soft delete records """
-        results = self._bulk_operation(object_name=self.object_name, operation='delete', data=data)
+        results = self._bulk_operation(object_name=self.object_name,
+                                       operation='delete', data=data)
         return results
 
     def insert(self, data):
         """ insert records """
-        results = self._bulk_operation(object_name=self.object_name, operation='insert', data=data)
+        results = self._bulk_operation(object_name=self.object_name,
+                                       operation='insert', data=data)
         return results
 
     def upsert(self, data, external_id_field):
         """ upsert records based on a unique identifier """
-        results = self._bulk_operation(object_name=self.object_name, operation='upsert', external_id_field=external_id_field, data=data)
+        results = self._bulk_operation(object_name=self.object_name,
+                                       operation='upsert',
+                                       external_id_field=external_id_field,
+                                       data=data)
         return results
 
     def update(self, data):
         """ update records """
-        results = self._bulk_operation(object_name=self.object_name, operation='update', data=data)
+        results = self._bulk_operation(object_name=self.object_name,
+                                       operation='update', data=data)
         return results
 
     def hard_delete(self, data):
         """ hard delete records """
-        results = self._bulk_operation(object_name=self.object_name, operation='hardDelete', data=data)
+        results = self._bulk_operation(object_name=self.object_name,
+                                       operation='hardDelete', data=data)
         return results
 
     def query(self, data):
         """ bulk query """
-        results = self._bulk_operation(object_name=self.object_name, operation='query', data=data)
+        results = self._bulk_operation(object_name=self.object_name,
+                                       operation='query', data=data)
         return results
 
-# TODO: refactor _call_salesforce, _exception_handler, and exception classes into util.py for common access between different API handlers
+# TODO: refactor _call_salesforce, _exception_handler,
+#       and exception classes into util.py for common
+#       access between different API handlers
 
 def _call_salesforce(url, method, session, headers, **kwargs):
     """Utility method for performing HTTP call to Salesforce.
