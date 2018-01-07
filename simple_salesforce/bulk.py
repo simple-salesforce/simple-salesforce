b7ce6adb1c67edd367229f2feac6b0e16185ab18
mbr84 <matthew.bramfeld@gmail.com>
2018-01-07 09:05:00 -0800

    styling and a bad import fixed

--- simple_salesforce/bulk.py
+++ simple_salesforce/bulk.py
@@ -6,6 +6,11 @@ try:
 except ImportError:
     # Python < 2.7
     from ordereddict import OrderedDict
+
+try:
+    from queue import Queue
+except: ImportError:
+    # Python < 3.x
     from Queue import Queue
 
 from threading import Thread
@@ -189,6 +194,13 @@ class SFBulkType(object):
                                external_id_field=external_id_field)
 
         def worker():
+            """ Add batches and wait for network responses (I/O) concurrently
+            with worker threads. (there may be a lot of batches)
+
+            Arguments: 
+            (Workers pull their args from the queue)
+            * data -- Batch-sized (<=10000 records) slice of list of dict, see line 185
+            """
             while True:
                 data = queue.get()
                 if data is None: break
@@ -201,13 +213,13 @@ class SFBulkType(object):
                     status = self._get_batch(job_id=batch['jobId'],
                                              batch_id=batch['id'])['state']
 
-                batch_results = self._get_batch_results(job_id=batch['jobId'], batch_id=batch['id'], 
-                                                        operation=operation)
+                batch_results = self._get_batch_results(job_id=batch['jobId'],
+                                            batch_id=batch['id'], operation=operation)
                 results.append(batch_results)
                 queue.task_done()
 
-        threads = [ Thread(target=worker) for n in range(num_threads) ]
-        for t in threads: t.start()
+        threads = [Thread(target=worker) for _ in range(num_threads)]
+        for thread in threads: thread.start()
 
         for j, i in enumerate(range(num_batches), 1):
             queue.put(data[i * 10000:j * 10000])
@@ -215,7 +227,7 @@ class SFBulkType(object):
         queue.join()
         for i in range(num_threads):
             queue.put(None)
-        for t in threads: t.join()
+        for thread in threads: thread.join()
 
         self._close_job(job_id=job['id'])
         return results
