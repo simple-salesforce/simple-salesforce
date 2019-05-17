e399fd14c9897e207bf33f771c5dec3527c7b63e
Marciel Torres <marcielribeirotorres@gmail.com>
2019-05-17 16:06:36 -0300

    Updating the requests lib to use 2.22.0 version or greater
    Why?
    The urllib3 library before 1.24.2 for Python mishandles certain cases where the desired set of CA certificates is different from the OS store of CA certificates, which results in SSL connections succeeding in situations where a verification failure is the correct outcome. This is related to use of the ssl_context, ca_certs, or ca_certs_dir argument.
    
    The Requests(2.22.0) lib uses:
    urllib3>=1.21.1,<1.26,!=1.25.0,!=1.25.1
    
    https://nvd.nist.gov/vuln/detail/CVE-2019-11324

--- setup.py
+++ setup.py
@@ -30,21 +30,21 @@ setup(
     author_email=about['__author_email__'],
     maintainer=about['__maintainer__'],
     maintainer_email=about['__maintainer_email__'],
-    packages=['simple_salesforce',],
+    packages=['simple_salesforce'],
     url=about['__url__'],
     license=about['__license__'],
     description=about['__description__'],
     long_description=textwrap.dedent(open('README.rst', 'r').read()),
 
     install_requires=[
-        'requests[security]'
+        'requests>=2.22.0'
     ] + pyver_install_requires,
     tests_require=[
         'nose>=1.3.0',
         'pytz>=2014.1.1',
         'responses>=0.5.1',
     ] + pyver_tests_require,
-    test_suite = 'nose.collector',
+    test_suite='nose.collector',
 
     keywords=about['__keywords__'],
     classifiers=[
