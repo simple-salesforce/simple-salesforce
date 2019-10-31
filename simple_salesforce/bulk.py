2228ad7f7a5717f32997373cebf6b6d637cde6d9
JonWobken <49450080+Mer-xes@users.noreply.github.com>
2019-10-31 15:19:40 -0500

    Update bulk.py

--- simple_salesforce/bulk.py
+++ simple_salesforce/bulk.py
@@ -210,7 +210,7 @@ class SFBulkType(object):
             job = self._create_job(object_name=object_name, operation=operation,
                                    external_id_field=external_id_field)
             chunked_data = [[i] for i in [data[i*batchsize:(i+1)*batchsize]
-                                          for i 
+                                          for i
                                           in range((len(data)
                                                     //batchsize+1))]]
 
