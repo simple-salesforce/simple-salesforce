5e03be83846ec80d59c9b3f96204a88cfa9ea9e2
Samuel Reyes <46464520+samuel-reyes@users.noreply.github.com>
2020-02-27 14:40:15 -0700

    Update api.py

--- simple_salesforce/api.py
+++ simple_salesforce/api.py
@@ -710,7 +710,7 @@ class SFType(object):
         * headers -- a dict with additional request headers.
         """
         # Append the external key to the end of our URL. Then remove it because Salesforce requires it not be present in the actual upsert. #
-        full_url=urljoin(self.base_url, external_key) + '/' + data[external_key]
+        full_url = urljoin(self.base_url, external_key) + '/' + data[external_key]
         del data[external_key]
         result = self._call_salesforce(
             method='PATCH', url=full_url,
