6505e1fef7ea56fbe72fb432a8b4e5b8c79e2209
Kimmo Ahokas <kimmo.ahokas@futurice.com>
2020-11-24 13:44:53 +0200

    Fix timezone handling in login
    Make sure all time related functions use timezone-aware datetime objects
    in UTC timezone. This fixes login issues on systems where local time is
    something else than UTC.

--- simple_salesforce/login.py
+++ simple_salesforce/login.py
@@ -6,9 +6,8 @@ Heavily Modified from RestForce 1.0.0
 DEFAULT_CLIENT_ID_PREFIX = 'RestForce'
 
 
-import time
 import warnings
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from html import escape
 from json.decoder import JSONDecodeError
 
@@ -154,14 +153,13 @@ def SalesforceLogin(
             consumer_key is not None and \
             privatekey_file is not None:
         header = {'alg': 'RS256'}
-        expiration = datetime.utcnow() + timedelta(minutes=3)
+        expiration = datetime.now(timezone.utc) + timedelta(minutes=3)
         payload = {
             'iss': consumer_key,
             'sub': username,
             'aud': 'https://{domain}.salesforce.com'.format(domain=domain),
             'exp': '{exp:.0f}'.format(
-                exp=time.mktime(expiration.timetuple()) +
-                    expiration.microsecond / 1e6
+                exp=expiration.timestamp()
             )
         }
         with open(privatekey_file, 'rb') as key:
