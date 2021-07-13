04e656aba60aebd030f00bdb41f47466dc7995d8
Tami Takamiya <ttakamiya@salesforce.com>
2021-07-12 22:40:53 -0400

    Support non-standard https port number

--- simple_salesforce/api.py
+++ simple_salesforce/api.py
@@ -143,6 +143,8 @@ class Salesforce:
             # interface for example) extract the hostname (which we rely on)
             if instance_url is not None:
                 self.sf_instance = urlparse(instance_url).hostname
+                port = urlparse(instance_url).port
+                if port != 443: self.sf_instance += ':' + str(port)
             else:
                 self.sf_instance = instance
 
