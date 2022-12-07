4feaa0f4dfd9975487cd93b11640ebe0f2557f6a
JonWobken <49450080+jon-wobken@users.noreply.github.com>
2022-12-07 14:22:27 -0600

    Update test_login.py
    typo in password arg

--- simple_salesforce/tests/test_login.py
+++ simple_salesforce/tests/test_login.py
@@ -235,7 +235,7 @@ class TestSalesforceLogin(unittest.TestCase):
     def test_connected_app_login_success(self):
         """Test a successful connected app login with a key file"""
         login_args = {
-            'passsword': 'password',
+            'password': 'password',
             'consumer_key': '12345.abcde',
             'consumer_secret': '12345.abcde'
             }
