0297abd4c245323b1bbd90396be99a58d996ab69
JonWobken <49450080+jon-wobken@users.noreply.github.com>
2023-01-17 09:45:15 -0600

    Update setup.py
    Update pyjwt dependency

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         },
 install_requires = [
                        'requests>=2.22.0',
-                       'pyjwt',
+                       'pyjwt[crypto]',
                        'zeep'
                        ],
                    tests_require = [
