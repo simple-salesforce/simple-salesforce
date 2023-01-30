d9713a42f8b7b504669f8e18af68f55b354e7eed
HAROLDO BONETTE CARVALHO <haroldo.bonette_carvalho@mercedes-benz.com>
2023-01-30 15:25:45 +0100

    no-else-return

--- simple_salesforce/api.py
+++ simple_salesforce/api.py
@@ -681,15 +681,15 @@ class Salesforce:
 
     def parse_result_to_json(self, result):
         """"Parse json from a Response object"""
-
-        #resources like sobjects/EventLogFile/[id]/LogFile are not in json format so they need to be converted from base64 first
+        # resources like sobjects/EventLogFile/[id]/LogFile \
+        # are not in json format so they need to be converted from base64 first
         if 'Content-Type' in result.headers and 'text/csv' in \
                 result.headers['Content-Type'].split(';'):
             return json.loads(self.csv_to_json(result.content.decode("utf-8")),
                             object_pairs_hook=self._object_pairs_hook, 
                             parse_float=self._parse_float)
-        else:
-            return result.json(object_pairs_hook=self._object_pairs_hook,
+        
+        return result.json(object_pairs_hook=self._object_pairs_hook,
                            parse_float=self._parse_float)
 
 
