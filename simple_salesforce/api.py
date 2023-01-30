cd7166a8e37ba459888f15a3ca49060f483b3fb5
HAROLDO BONETTE CARVALHO <haroldo.bonette_carvalho@mercedes-benz.com>
2023-01-30 15:35:03 +0100

    static checks

--- simple_salesforce/api.py
+++ simple_salesforce/api.py
@@ -677,7 +677,8 @@ class Salesforce:
         """"Converts a CSV string into json format using Pandas"""
         csvStringIO = StringIO(csvString)
         df = pd.read_csv (csvStringIO)
-        return df.to_json(orient = 'index') # dict like {index -> {column -> value}}
+        # returns a dict like {index -> {column -> value}}
+        return df.to_json(orient = 'index') 
 
     def parse_result_to_json(self, result):
         """"Parse json from a Response object"""
@@ -686,7 +687,7 @@ class Salesforce:
         if 'Content-Type' in result.headers and 'text/csv' in \
                 result.headers['Content-Type'].split(';'):
             return json.loads(self.csv_to_json(result.content.decode("utf-8")),
-                            object_pairs_hook=self._object_pairs_hook, 
+                            object_pairs_hook=self._object_pairs_hook,
                             parse_float=self._parse_float)
         
         return result.json(object_pairs_hook=self._object_pairs_hook,
