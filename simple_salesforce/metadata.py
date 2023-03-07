2448aab9335c99d4c62235e4bad48ef69891b41d
Mahipal Singh <mahipal@github.com>
2023-03-07 11:21:08 +0530

    Added zip file changes

--- simple_salesforce/metadata.py
+++ simple_salesforce/metadata.py
@@ -264,7 +264,7 @@ class SfdcMetadataApi:
             'client': client,
             'checkOnly': checkOnly,
             'sessionId': self._session_id,
-            'ZipFile': self._read_deploy_zip(zipfile),
+            'ZipFile': zipfile,
             'testLevel': testLevel,
             'tests': tests,
             'ignoreWarnings': ignoreWarnings,
